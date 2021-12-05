a9c184b254f5d6000bd03b5eebdeab149cf6363a
Elena Carasec <elena.carasec@gmail.com>
2021-12-05 17:14:21 +0100

    homepage

--- ComforteZoneApp/App.js
+++ ComforteZoneApp/App.js
@@ -16,7 +16,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-        <Stack.Navigator initialRouteName="HomePage" screenOptions={{headerShown: false}}>
+        <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="EntryTest" component={EntryTest} />
